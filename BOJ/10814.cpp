5f242bb44a8dc9194750c0c817bbbc8f3aaa2bf4
orca <35363038+lsjtop10@users.noreply.github.com>
2025-11-17 00:42:13 +0000

    Solve BOJ 10814.cpp

---
+++ BOJ/10814.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct User{
+    int Age;
+    string Name;
+};
+
+bool compare(User a, User b){
+    return a.Age < b.Age;
+}
+
+int main(){
+    //stdlib 사용하는 방식으로 구현
+    int num;
+    cin >> num;
+
+    vector<User> data(num);
+
+    for(int i = 0; i < num; i++){
+        cin >> data[i].Age;
+        cin >> data[i].Name;
+    }
+
+
+    stable_sort(data.begin(), data.end(), compare);
+
+    for(int i = 0; i < data.size(); i++){
+        cout << data[i].Age << " " << data[i].Name << "\n";
+    }
+}
