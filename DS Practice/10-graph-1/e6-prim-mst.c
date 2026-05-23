f067e196caef18560c96f677581e7f4fecc192d0
orca <dev.orca04@gamil.com>
2026-05-23 10:42:14 +0900

    feat(graph): implement Prim's algorithm for Minimum Spanning Tree (MST) using adjacency matrix

---
+++ DS Practice/10-graph-1/e6-prim-mst.c
@@ -0,0 +1,65 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INF 1000
+#define N 7
+
+char vName[N] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
+int graph[N][N] = {{0, 29, INF, INF, INF, 10, INF}, {25, 0, 16, INF, INF, INF, 15}, {INF, 16, 0, 12, INF, INF, 16},
+                   {INF, INF, 12, 0, 22, INF, 18},  {INF, INF, INF, 22, 0, 27, 25}, {10, INF, INF, INF, 27, 0, INF},
+                   {INF, 15, INF, 18, 25, INF, 0}};
+
+bool visited[N];
+int dist[N];
+
+void init() {
+    for (int i = 0; i < N; i++) {
+        dist[i] = INF;
+        visited[i] = false;
+    }
+}
+
+int getMinVertex() {
+    int minVertex = 0;
+    int minDist = INF;
+
+    for (int i = 0; i < N; i++) {
+        if (visited[i] == false && dist[i] < minDist) {
+            minVertex = i;
+            minDist = dist[i];
+        }
+    }
+
+    return minVertex;
+}
+
+void prim(int startVNum) {
+    dist[startVNum] = 0;
+
+    for (int i = 0; i < N; i++) {
+        int vNum = getMinVertex();
+        visited[vNum] = true;
+
+        printf("[%c, %d]", vName[vNum], dist[vNum]);
+
+        for (int aNum = 0; aNum < N; aNum++) {
+            if (visited[aNum] == false &&
+                // TODO: 여기에서 dist[vNum]이 왜 안 들어가는지 확인 필요
+                //  아 이거 최단경로 찾는 알고리즘이 아니지.. 
+                /*dist[vNum]*/ +graph[vNum][aNum] < dist[aNum]) {
+                dist[aNum] = /*dist[vNum]*/ +graph[vNum][aNum];
+            }
+        }
+    }
+}
+
+int main(void) {
+    init();
+    prim(2);
+}
